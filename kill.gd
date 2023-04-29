40be5a1fd659924c2cead5f275e03cac904893d4
LancersBucket <andrewonline00@gmail.com>
2023-04-29 14:54:28 -0400

    Implemented killzones to work with enemies

--- kill.gd
+++ kill.gd
@@ -14,3 +14,6 @@ func _process(delta):
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		get_tree().reload_current_scene()
+	elif body.is_in_group("Enemy"):
+		body.queue_free()
+		print("Deleted Enemy")
