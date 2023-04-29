9f6c9d60f373c515d63569b49668f0d95db9de81
AndrewAwesome100 <professor.ajr20@gmail.com>
2023-04-28 22:49:14 -0400

    Fixed goomba - Simplified player interaction - Fixed goombas sticking to each other Moved character in main scene (no impact, just mentioning)

--- goomba.gd
+++ goomba.gd
@@ -9,6 +9,7 @@ var isOnScreen = 0
 var alive = 1
 var x = 0
 var direction = -1
+var cooldown = 0
 
 func _physics_process(delta):
 	if isOnScreen && alive:
@@ -16,9 +17,13 @@ func _physics_process(delta):
 		if not is_on_floor():
 			velocity.y += gravity * delta
 
-		if is_on_wall():
+		if is_on_wall() && cooldown == 0:
 			direction *= -1
+			cooldown = 2
 		
+		if cooldown > 0:
+			cooldown -= 1
+			
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.play("walk")
@@ -33,19 +38,18 @@ func die():
 	$AnimatedSprite2D.play("die")
 	get_node("CollisionShape2D").set_deferred("disabled",true)
 	get_node("Area2D/CollisionShape2D2").set_deferred("disabled",true)
-	get_node("Area2D2/CollisionShape2D3").set_deferred("disabled",true)
 
 	alive = 0
 	
 	await get_tree().create_timer(.5).timeout
 	queue_free()
-
-func _on_area_2d_2_body_entered(body):
-	if body.is_in_group("Player"):
-		player.velocity.y = -200
-		die()
+	
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().reload_current_scene()
+		if player.velocity.y > 0:
+			die()
+			player.velocity.y = -200
+		else:
+			get_tree().reload_current_scene()
 
