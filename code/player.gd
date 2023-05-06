a0ba27bce5bad7ced32d308a123edf834da0f60d
LancersBucket <andrewonline00@gmail.com>
2023-05-06 14:33:54 -0400

    Adjusted player movement code to be more standard Fixed bugs related to file tree restructure

--- code/player.gd
+++ code/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-@export var run_speed = 175
 @export var walk_speed = 100
 @export var jump_speed = -350.0
 
@@ -28,79 +27,28 @@ func _ready():
 	
 func _physics_process(delta):
 	if (playerControl):
-		# Add the gravity.
+		# Gravity
 		velocity.y += gravity * delta
 		
-		if !is_on_floor():
-			timer -= 1
-		else:
-			timer = 7
+		# Player jump grace period
+		if !is_on_floor(): timer -= 1
+		else: timer = 7
 		
-		# Handle Jump.
+		# Handle jump
 		if Input.is_action_just_pressed("move_up") and timer >= 0:
 			velocity.y = jump_speed
 			$"boing-boing".play()
 		
-		# Get the input direction.
+		# Get the input direction
 		var direction = Input.get_axis("move_left", "move_right")
 		
-		# Whole bunch of code to calculate momentum
-		if (is_on_floor()):
-			# If on floor and running
-			if Input.is_action_pressed("run"):
-				if direction == RIGHT:
-					velocity.x = min(velocity.x+acceleration, run_speed)
-				elif direction == LEFT:
-					velocity.x = max(velocity.x-acceleration, -run_speed)
-				else:
-					# Slowly brings velocity to zero if not touching anything
-					velocity.x = lerp(velocity.x, 0.0, friction)
-			# If on floor and walking
-			elif !Input.is_action_pressed("run"):
-				# If speed is > walk_speed decelerate to walk_speed 
-				if (velocity.x > walk_speed+10):
-					velocity.x = lerp(velocity.x, float(walk_speed), friction)
-				else:
-					if direction == RIGHT:
-						velocity.x = min(velocity.x+acceleration, walk_speed)
-					elif direction == LEFT:
-						velocity.x = max(velocity.x-acceleration, -walk_speed)
-					else:
-						# Slowly brings velocity to zero if not touching anything
-						velocity.x = lerp(velocity.x, 0.0, friction)
-		elif !is_on_floor():
-			# If not on floor and running
-			if Input.is_action_pressed("run"):
-				if direction == RIGHT:
-					velocity.x = min(velocity.x+(acceleration/2.0), run_speed)
-				elif direction == LEFT:
-					velocity.x = max(velocity.x-(acceleration/2.0), -run_speed)
-			# If not on floor and walking
-			elif !Input.is_action_pressed("run"):
-				if velocity.x < walk_speed:
-					if direction == RIGHT:
-						velocity.x = min(velocity.x+(acceleration/2.0), walk_speed)
-					elif direction == LEFT:
-						velocity.x = max(velocity.x-(acceleration/2.0), -walk_speed)
-		
-		# If your running enable higher jumps
-		if (Input.is_action_pressed("run") && is_on_floor() && velocity.x > walk_speed):
-			jump_speed = -375
-		else:
-			jump_speed = -350
-		
-		# Prevents direction from going 0 and setting velocity.x instantly to 0
-		if is_on_floor():
-			if direction == LEFT:
-				facing = LEFT
-			elif direction == RIGHT:
-				facing = RIGHT
+		# Movement speed
+		velocity.x = walk_speed*direction
 		
 		move_and_slide()
 		
 		# Clamps player position to stay on screen
 		position.x = maxf(position.x, camera.position.x-(screen_size.x/2)-24)
-		
 		
 		# Animation
 		if velocity.y < 0:
@@ -111,7 +59,7 @@ func _physics_process(delta):
 			$AnimatedSprite2D.animation = "down"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = velocity.x < 0
-		elif velocity.x != 0 && abs(velocity.x) > 15 :
+		elif velocity.x != 0:
 			$AnimatedSprite2D.animation = "walk"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = velocity.x < 0
@@ -120,37 +68,6 @@ func _physics_process(delta):
 			$AnimatedSprite2D.animation = "idle"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.play("idle")
-	# Idealy I would have implemented this into a state machine but I don't particuarally care
-	elif (!playerControl && flagControl):
-		velocity.x = 0
-		if (!is_on_floor() && !lock):
-			velocity.x = 0
-			velocity.y = 64
-			temp = position.x
-			$AnimatedSprite2D.animation = "slide"
-		elif (is_on_floor() || lock):
-			if (!lock):
-				position.x = temp + 28
-				$AnimatedSprite2D.flip_h = true
-				await get_tree().create_timer(.5).timeout
-				lock = true
-				velocity.x = 0
-			else:
-				$AnimatedSprite2D.animation = "walk"
-				$AnimatedSprite2D.flip_v = false
-				$AnimatedSprite2D.flip_h = velocity.x < 0
-				$AnimatedSprite2D.play("walk")
-				velocity.y += gravity*delta
-				velocity.x += walk_speed/2.0
-				
-				if (is_on_wall()):
-					velocity.x = 0
-					$AnimatedSprite2D.stop()
-					await get_tree().create_timer(3).timeout
-					die()
-			
-		move_and_slide()
-		
 		
 	# Camera position code. Sets camera to player x except when player is behind camera x
 	#if (position.x > camera.position.x):
@@ -166,4 +83,4 @@ func clampNum(_num, _min, _max):
 	
 func die():
 	lives -= 1
-	get_tree().change_scene_to_file("res://info_screen.tscn")
+	get_tree().change_scene_to_file("res://scenes/info_screen.tscn")
