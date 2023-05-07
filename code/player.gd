5bea94dc766e396d2b596e0ad53879003b4b8c04
LancersBucket <andrewonline00@gmail.com>
2023-05-06 20:04:26 -0400

    Added spike and stun state for player

--- code/player.gd
+++ code/player.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @export var sneak_speed = walk_speed/2.0
 @export var jump_speed = -350.0
 
-enum States {PLAYER_CONTROL}
+enum States {PLAYER_CONTROL, STUN}
 enum MovementStates {NORMAL, WALL_JUMP, WALL_GRAB}
 const NORMAL = Vector2(0,-1)
 const RIGHT = 1
@@ -19,6 +19,10 @@ var graceTimer = 7
 var lives = 3
 var playerState = States.PLAYER_CONTROL
 var movementState = MovementStates.NORMAL
+var stunTimerLength = 200
+var stunTimer = stunTimerLength
+var facing = RIGHT
+
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -38,7 +42,7 @@ func _physics_process(delta):
 			#$"JumpSFX".play()
 		elif Input.is_action_just_pressed("move_up") and is_on_wall_only():
 			velocity.y = jump_speed
-			velocity.x = -walk_speed
+			velocity.x = walk_speed*facing
 			movementState = MovementStates.WALL_JUMP
 			
 		# Get the input direction
@@ -56,6 +60,11 @@ func _physics_process(delta):
 			if (is_on_floor()):
 				movementState = MovementStates.NORMAL
 		
+		if direction == RIGHT:
+			facing = RIGHT
+		elif direction == LEFT:
+			facing = LEFT
+		
 		move_and_slide()
 		
 		
@@ -65,19 +74,36 @@ func _physics_process(delta):
 		# Animation
 		if velocity.y < 0:
 			$AnimatedSprite2D.flip_v = false
-			$AnimatedSprite2D.flip_h = velocity.x < 0
+			$AnimatedSprite2D.flip_h = -min(0,facing)
 			$AnimatedSprite2D.animation = "up"
 		elif velocity.y > 0:
 			$AnimatedSprite2D.flip_v = false
-			$AnimatedSprite2D.flip_h = velocity.x < 0
+			$AnimatedSprite2D.flip_h = -min(0,facing)
 			$AnimatedSprite2D.animation = "down"
 		elif velocity.x != 0:
 			$AnimatedSprite2D.flip_v = false
-			$AnimatedSprite2D.flip_h = velocity.x < 0
+			$AnimatedSprite2D.flip_h = -min(0,facing)
 			$AnimatedSprite2D.play("walk") 
 		else:
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.play("idle")
+			
+	if (playerState == States.STUN):
+		if (is_on_floor() && stunTimer <= stunTimerLength-1):
+			velocity.x = 0
+			stunTimer -= 1
+		if (stunTimer == stunTimerLength):
+			velocity.y = jump_speed
+			velocity.x = walk_speed*-facing
+			stunTimer -= 1
+		elif (stunTimer <= 0):
+			stunTimer = stunTimerLength
+			playerState = States.PLAYER_CONTROL
+		
+		velocity.y += gravity * delta
+		print(stunTimer)
+		move_and_slide()
+		
 		
 	# Camera position code. Sets camera to player x except when player is behind camera x
 	#if (position.x > camera.position.x):
