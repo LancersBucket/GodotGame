262c17c7f897b6bc06487ec017f3f0ed3b571da3
LancersBucket <andrewonline00@gmail.com>
2023-05-06 20:31:48 -0400

    Added falling spike

---
+++ code/falling_object.gd
@@ -0,0 +1,23 @@
+extends RigidBody2D
+
+@onready var player = $"/root/Main/Player"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Player"):
+		gravity_scale = .5
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		player.playerState = player.States.STUN
+		player.stunTimer = player.stunTimerLength
+		player.velocity.x = 0
+		player.velocity.y = 0
