57b0b95e672b8364456fef826e79fb3a0873fbdc
LancersBucket <andrewonline00@gmail.com>
2023-05-10 17:33:50 -0400

    Despawn timer fix

--- code/projectile.gd
+++ code/projectile.gd
@@ -16,7 +16,8 @@ func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		player.playerState = player.States.STUN
 		player.stunTimer = player.stunTimerLength
-	$DespawnTimer.start()
+	if !body.is_in_group("Player"):
+		$DespawnTimer.start()
 
 
 
