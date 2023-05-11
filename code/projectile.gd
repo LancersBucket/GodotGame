9c7e5aca8da1018813dcd4411e6a880726d06646
LancersBucket <andrewonline00@gmail.com>
2023-05-10 21:30:06 -0400

    Added customizable projectile fade out

--- code/projectile.gd
+++ code/projectile.gd
@@ -1,8 +1,13 @@
 extends StaticBody2D
 
 @onready var player = $"/root/Main/Player"
+
+@export var fadeOutTime = 30.0
+
 var speed = .5
 var falling = true
+var delete = false
+var deleteFade = fadeOutTime
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,6 +17,11 @@ func _ready():
 func _process(delta):
 	if (falling):
 		var temp = move_and_collide(Vector2(0,speed))
+	if (delete):
+		deleteFade -= 1
+		modulate = Color(1, 1, 1, deleteFade/fadeOutTime)
+	if (deleteFade <= 0):
+		queue_free()
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
@@ -26,4 +36,5 @@ func _on_area_2d_body_entered(body):
 
 
 func _on_despawn_timer_timeout():
-	queue_free()
+	delete = true
+	#queue_free()
