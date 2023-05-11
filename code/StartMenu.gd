9f2915a8412e0772ad6b27756ddfcd32345da2a4
AndrewAwesome100 <professor.ajr20@gmail.com>
2023-05-10 20:54:34 -0400

    Updated start menu

--- code/StartMenu.gd
+++ code/StartMenu.gd
@@ -1,7 +1,7 @@
 extends Control
 
-@onready var quit = $CenterContainer/VBoxContainer/HBoxContainer/Quit
-@onready var start = $CenterContainer/VBoxContainer/HBoxContainer/Start
+@onready var quit = $CenterContainer/VBoxContainer/HBoxContainer/VBoxContainer/Quit
+@onready var start = $CenterContainer/VBoxContainer/HBoxContainer/VBoxContainer/Start
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
