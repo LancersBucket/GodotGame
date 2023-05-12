88d289cae39c591dbd51a822d483cfa23769958a
Jackpanarella12 <jackpanarella1212@gmail.com>
2023-05-11 20:21:04 -0400

    added sfx to pause menu (except for exiting)

--- code/pause_menu.gd
+++ code/pause_menu.gd
@@ -4,14 +4,25 @@ extends ColorRect
 @onready var exit = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Exit
 @onready var animator: AnimationPlayer = $AnimationPlayer
 
+
+func playSound():
+	$"BeepSFX".play()
 func _ready():
 	resume.pressed.connect(unpause)
 	exit.pressed.connect(get_tree().quit)
+	resume.mouse_entered.connect(playSound)
+	exit.mouse_entered.connect(playSound)
 
 func unpause():
+	$"Beep2SFX".play()
+	$"BeepSFX".stop()
 	animator.play("Unpause")
 	get_tree().paused = false
+	$"Beep2SFX".stop()
 
 func pause():
+	$"BeepSFX".stop()
+	$"Beep2SFX".stop()
+	$"Beep2SFX".play()
 	animator.play("Pause")
 	get_tree().paused = true
