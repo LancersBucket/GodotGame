7da888e58d3ae0901bb240543e70965d3a5aebd2
Jackpanarella12 <jackpanarella1212@gmail.com>
2023-05-11 21:06:13 -0400

    fixed funny

--- code/pause_menu.gd
+++ code/pause_menu.gd
@@ -3,26 +3,34 @@ extends ColorRect
 @onready var resume = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Resume
 @onready var exit = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Exit
 @onready var animator: AnimationPlayer = $AnimationPlayer
-
-
+@onready var paused = false
 func playSound():
-	$"BeepSFX".play()
+	if paused:
+		$"BeepSFX".play()
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	exit.disabled = true
 	resume.pressed.connect(unpause)
 	exit.pressed.connect(get_tree().quit)
 	resume.mouse_entered.connect(playSound)
 	exit.mouse_entered.connect(playSound)
 
 func unpause():
-	$"Beep2SFX".play()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	paused = false
 	$"BeepSFX".stop()
 	animator.play("Unpause")
 	get_tree().paused = false
-	$"Beep2SFX".stop()
+	exit.disabled = true
 
 func pause():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	paused = true
 	$"BeepSFX".stop()
 	$"Beep2SFX".stop()
 	$"Beep2SFX".play()
 	animator.play("Pause")
 	get_tree().paused = true
+	exit.disabled = false
+	
+
