bb25734988b76f5e0e730ca630aa4bbc66a7f701
LancersBucket <andrewonline00@gmail.com>
2023-05-14 14:08:53 -0400

    Removed quit button from both pause and main menu (Since it'll be a web export there is no need for it)

--- code/pause_menu.gd
+++ code/pause_menu.gd
@@ -1,7 +1,7 @@
 extends ColorRect
 
 @onready var resume = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Resume
-@onready var exit = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Exit
+#@onready var exit = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Exit
 @onready var animator: AnimationPlayer = $AnimationPlayer
 @onready var paused = false
 func playSound():
@@ -9,11 +9,11 @@ func playSound():
 		$"BeepSFX".play()
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	exit.disabled = true
+	#exit.disabled = true
 	resume.pressed.connect(unpause)
-	exit.pressed.connect(get_tree().quit)
+	#exit.pressed.connect(get_tree().quit)
 	resume.mouse_entered.connect(playSound)
-	exit.mouse_entered.connect(playSound)
+	#exit.mouse_entered.connect(playSound)
 
 func unpause():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -21,7 +21,7 @@ func unpause():
 	$"BeepSFX".stop()
 	animator.play("Unpause")
 	get_tree().paused = false
-	exit.disabled = true
+	#exit.disabled = true
 
 func pause():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -31,6 +31,6 @@ func pause():
 	$"Beep2SFX".play()
 	animator.play("Pause")
 	get_tree().paused = true
-	exit.disabled = false
+	#exit.disabled = false
 	
 
